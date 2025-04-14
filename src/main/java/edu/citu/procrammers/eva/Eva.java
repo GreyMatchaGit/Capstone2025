e686ef2616048e29137a0edb44fec87e592b7de8
GreyMatchaGit <greymatcha@proton.me>
2025-04-14 16:03:21 +0800

    Added navigation service

---
+++ src/main/java/edu/citu/procrammers/eva/Eva.java
@@ -0,0 +1,23 @@
+package edu.citu.procrammers.eva;
+
+import edu.citu.procrammers.eva.utils.NavService;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import static edu.citu.procrammers.eva.utils.Constant.Page.*;
+
+public class Eva extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        new NavService(primaryStage);
+        primaryStage.show();
+        NavService.navigateTo(
+            Splash
+        );
+    }
+}
---
+++ src/main/java/edu/citu/procrammers/eva/utils/Constant.java
@@ -0,0 +1,8 @@
+package edu.citu.procrammers.eva.utils;
+
+public class Constant {
+
+    public static class Page {
+        public static final String Splash = "splash-view.fxml";
+    }
+}
