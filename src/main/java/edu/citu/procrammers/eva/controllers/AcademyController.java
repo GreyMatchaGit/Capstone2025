b32816712267d2eaac71e0879b9c79f4c3330c9c
Clorky <clarkisnoob434@gmail.com>
2025-05-04 15:50:19 +0800

    Implemented fetching from tbllessons for the list of lessons.

--- src/main/java/edu/citu/procrammers/eva/controllers/AcademyController.java
+++ src/main/java/edu/citu/procrammers/eva/controllers/AcademyController.java
@@ -1,15 +1,18 @@
 package edu.citu.procrammers.eva.controllers;
 
+import edu.citu.procrammers.eva.data.Database;
 import edu.citu.procrammers.eva.data.LessonContent;
 import edu.citu.procrammers.eva.utils.NavService;
 import edu.citu.procrammers.eva.utils.SoundManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static edu.citu.procrammers.eva.utils.Constant.Page.*;
@@ -17,19 +20,27 @@ import static edu.citu.procrammers.eva.utils.UIElementUtils.setupGlow;
 
 public class AcademyController {
     private ArrayList<LessonContent> lessons = new ArrayList<>();
-    private int index = 0;
+    private int index = -2;
 
     public Pane fadePane;
+    public AnchorPane apPrefaceP1, apPrefaceP2;
     public ImageView imgLessonsBkmrk, imgTutorBkmrk, imgBackMenuBtn, imgSettingsBtn, imgBackBtn, imgNextBtn, imgViewVisualizer;
     public TextArea taDiscussion, taCodeSnippet;
-    public Text txtToppicTitle, txtTopicNum;
+    public Text txtTopicTitle, txtTopicNum;
 
     @FXML
     public void initialize() {
-
         SoundManager.playBackgroundMusic("music/academy_music.MP3", true);
 
+        try {
+            lessons =  Database.getInstance().loadLessons();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         setupGlow(imgLessonsBkmrk, imgTutorBkmrk, imgBackMenuBtn, imgSettingsBtn, imgBackBtn, imgNextBtn, imgViewVisualizer);
+
+        imgBackBtn.setVisible(false);
 
         imgBackMenuBtn.setOnMouseClicked(e -> {
             SoundManager.playSFX("sfx/btn_click.MP3");
@@ -42,19 +53,65 @@ public class AcademyController {
             NavService.navigateTo(Settings);
             NavService.previousPage = Academy;
         });
-
     }
 
     public void viewVisualizer() {
         NavService.navigateTo(lessons.get(index).getVisualizerPath());
     }
 
-    private void loadLesson() {
+    private void loadLessonContents() {
         LessonContent content = lessons.get(index);
+
         taDiscussion.setText(content.getLessonText());
         taCodeSnippet.setText(content.getCodeSnippet());
-        txtToppicTitle.setText(content.getTopicTitle());
+        txtTopicTitle.setText(content.getTopicTitle());
         txtTopicNum.setText("0" + (index + 1));
     }
 
+    public void nextLesson() {
+        SoundManager.playSFX("sfx/btn_click.MP3");
+        index++;
+
+        if (index == -1) {
+            imgBackBtn.setVisible(true);
+            apPrefaceP1.setVisible(false);
+            apPrefaceP1.setTranslateX(1500);
+            return;
+        } else if (index == 0) {
+            apPrefaceP2.setVisible(false);
+            apPrefaceP2.setTranslateX(1500);
+            imgBackBtn.setVisible(false);
+        }
+
+        imgNextBtn.setVisible(index + 1 < lessons.size());
+        if (index > 0 && index < lessons.size()) {
+            imgBackBtn.setVisible(true);
+        }
+
+        loadLessonContents();
+    }
+
+    public void prevLesson() {
+        SoundManager.playSFX("sfx/btn_click.MP3");
+        index--;
+
+        if (index == -2) {
+            imgBackBtn.setVisible(false);
+            apPrefaceP1.setVisible(true);
+            apPrefaceP1.setTranslateX(0);
+            return;
+        } else if (index == -1) {
+            apPrefaceP2.setVisible(true);
+            apPrefaceP2.setTranslateX(0);
+            return;
+        }
+
+
+        imgBackBtn.setVisible(index - 1 >= 0);
+        if (index > 0 && index < lessons.size()) {
+            imgNextBtn.setVisible(true);
+        }
+
+        loadLessonContents();
+    }
 }
--- src/main/java/edu/citu/procrammers/eva/data/LessonContent.java
+++ src/main/java/edu/citu/procrammers/eva/data/LessonContent.java
@@ -1,16 +1,22 @@
 package edu.citu.procrammers.eva.data;
 
 public class LessonContent {
+    private final int id;
     private final String topicTitle;
     private final String lessonText;
     private final String codeSnippet;
     private final String visualizerPath;
 
-    public LessonContent(String topicTitle, String lessonText, String codeSnippet, String visualizerPath) {
+    public LessonContent(int id, String topicTitle, String lessonText, String codeSnippet, String visualizerPath) {
+        this.id = id;
         this.topicTitle = topicTitle;
         this.lessonText = lessonText;
         this.codeSnippet = codeSnippet;
         this.visualizerPath = visualizerPath;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTopicTitle() {
