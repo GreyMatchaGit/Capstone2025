4bbeefff88061c015df8dbf426e3fbabc65e1448
GreyMatcha <greymatcha@proton.me>
2025-05-09 01:57:23 +0800

    Fixed comment

--- src/main/java/edu/citu/procrammers/eva/models/strategy/hashtable/CollisionStrategy.java
+++ src/main/java/edu/citu/procrammers/eva/models/strategy/hashtable/CollisionStrategy.java
@@ -1,9 +1,9 @@
 package edu.citu.procrammers.eva.models.strategy.hashtable;
 
 public interface CollisionStrategy {
-    /*
-     * Paramdam: index is initially the result of the compression function.
-     * Return: FINISHED if it found the index to insert the value in.
+    /**
+     * @Param: Index is initially the result of the compression function.
+     * @Return: FINISHED if it found the index to insert the value in.
      * Otherwise, return the next index to check for collision.
      */
     int handleCollision(int index);
