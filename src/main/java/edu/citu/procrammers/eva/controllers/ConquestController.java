a82266a6904bfde95179911f9665627b3dbe683d
Clorkies <clarkisnoob434@gmail.com>
2025-05-10 01:43:27 +0800

    Updated UI for conquest screen.

--- src/main/java/edu/citu/procrammers/eva/controllers/ConquestController.java
+++ src/main/java/edu/citu/procrammers/eva/controllers/ConquestController.java
@@ -1,13 +1,20 @@
 package edu.citu.procrammers.eva.controllers;
 
+import edu.citu.procrammers.eva.Eva;
+import edu.citu.procrammers.eva.data.Database;
+import edu.citu.procrammers.eva.data.User;
 import edu.citu.procrammers.eva.utils.NavService;
 import edu.citu.procrammers.eva.utils.SoundManager;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 import javafx.application.Platform;
+
+import java.awt.*;
 
 import static edu.citu.procrammers.eva.utils.Constant.Page.*;
 import static edu.citu.procrammers.eva.utils.UIElementUtils.setupGlow;
@@ -15,12 +22,31 @@ import static edu.citu.procrammers.eva.utils.UIElementUtils.setupGlow;
 public class ConquestController {
     public Pane fadePane;
     public ImageView imgBackMenuBtn, imgSettingsBtn;
+    public ProgressBar pbUserExpLevel;
+    public Label lblUserName, lblUserLevel;
+
+    private User currentUser = Eva.currentUser;
+
+    // Level/exp handling and tracking to be implemented
+    // Temporary variables for now
+    private final int level = 1;
+    private final double exp = 32.0;
 
     @FXML
     public void initialize() {
 
+        // For testing purposes
+        if (currentUser == null) {
+            currentUser = new User(999);
+            currentUser.username = "John Doe";
+        }
+
         SoundManager.playBackgroundMusic("music/conquest_music.MP3", true);
         setupGlow(imgBackMenuBtn, imgSettingsBtn);
+
+        lblUserName.setText(currentUser.username);
+        lblUserLevel.setText("Level " + level);
+        pbUserExpLevel.setProgress(exp / 100);
 
         imgBackMenuBtn.setOnMouseClicked(e -> {
             SoundManager.playSFX("sfx/btn_click.MP3");
